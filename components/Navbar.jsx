51066f74993bf97f222e3d2dd7c6a777a1eae451
Jamie Padovano <jp1483@messiah.edu>
2025-08-14 17:10:14 -0400

    navbar

---
+++ components/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from "react"
+import Link from "next/link";
+
+const Navbar = () => {
+  return (
+    <nav className="border-1 text-white p-5 rounded-full shadow-md max-w-4xl mx-auto">
+      <ul className="flex space-x-4">
+        <li>
+          <Link href="#about">About</Link>
+        </li>
+        <li>
+          <Link href="#projects">Projects</Link>
+        </li>
+        <li>
+          <Link href="#contact">Contact</Link>
+        </li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Navbar;
